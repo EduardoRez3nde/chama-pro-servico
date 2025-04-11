f47abc7d9532a9e7a5e08e9556ba5f0828dd4dd7
Eduardo Rezende <dudurezende426@gmail.com>
2025-04-11 17:36:03 -0400

    project started

---
+++ src/main/java/com/project/chama_pro_servico/ChamaProServicoApplication.java
@@ -0,0 +1,13 @@
+package com.project.chama_pro_servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaProServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamaProServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/chama_pro_servico/ChamaProServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chama_pro_servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamaProServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
